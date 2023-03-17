ff9168301d97a6e38deb12673fbed818f41c34d5
Fancydara <maryamalabi90@gmail.com>
2023-03-17 16:11:04 -0700

    digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-#include<stdio.h>
-/* betty style doc for function main goes there */
+
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: always 0
+ * A simple program to illustrate how if-else conditional statements
+ * are constructed in C.
+ *
+ * To find the last digit of a number, you can use the modulo operator (%).
+ * The modulo operator returns the remainder of a division operation. To find
+ * the last digit of a number, you can take the modulo of the number with 10.
+ * Here's the equation: last_digit = (number % 10)
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n;
-	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
-		printf("last digit of %d is %d and is grater than 5", n, m);
-	else if (m == 0)
-		printf("last digit of %d is %d and is 0", n, m);
-	else
-		printf("last digit of %d is %d and is less than 6 and not 0", n, m);
 
-	printf("\n");
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
+	} else if ((n % 10) == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, (n % 10));
+	} else if (((n % 10) < 6) && ((n % 10) != 0))
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
+	}
 
 	return (0);
 }
